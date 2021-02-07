e5cc46ba4d873301eb7b6a74484c80ee9a523ef3
leehyeokjae <leehyeokjae97@gmail.com>
2021-02-07 22:39:16 +0900

    ver 0.2.2
    delete test code

--- dist/modules/make_model.js
+++ dist/modules/make_model.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var brain = require("../../brainjs/brain.js");
 function create_ai_data(data) {
     var net = new brain.recurrent.LSTMTimeStep();
-    net.train([data.slice(0, 10)], {
+    net.train([data], {
         log: true,
         logPeriod: 10,
         iterations: 999999999999999,
--- src/modules/make_model.ts
+++ src/modules/make_model.ts
@@ -1,7 +1,7 @@
 const brain = require("../../brainjs/brain.js");
 export default function create_ai_data(data: any) {
   const net = new brain.recurrent.LSTMTimeStep();
-  net.train([data.slice(0, 10)], {
+  net.train([data], {
     log: true,
     logPeriod: 10,
     iterations: 999999999999999,
